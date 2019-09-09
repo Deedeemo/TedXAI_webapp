a890a7a37970163d2a7ad889582936efcdfe039f
Deedeemo <moga.andreea87@gmail.com>
2019-09-09 18:54:54 +0200

    Update server.py
    Replaced the GDrive link with an automatic download link

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1oDnqWLW6MKYbkoTNzWn9tcd0tAlDHjns'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1oDnqWLW6MKYbkoTNzWn9tcd0tAlDHjns'
 export_file_name = 'export.pkl'
 
 classes = ['Great leader', 'housekeeper']
